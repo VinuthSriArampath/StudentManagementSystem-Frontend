fffd20d0dcd30725624c113cbe48a3d540371918
XTshadowX <vinuthsriarampath@outlook.com>
2024-09-03 14:01:09 +0530

    added a table

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+let tablebody=document.getElementById("tablebody");
+let body=``;
+fetch("http://localhost:8080/student/all")
+.then(res => res.json())
+.then(data =>{
+    console.log(data);
+    data.forEach(element => {
+        body+=`                <tr>
+        <td>${element.id}</td>
+        <td>${element.fname}</td>
+        <td>${element.lname}</td>
+        <td>${element.email}</td>
+        <td>${element.age}</td>
+        <td>${element.grade}</td>
+        <td>${element.phone}</td>
+    </tr>`
+    });
+    tablebody.innerHTML=body
+});
+
+
+
+
 loadcontent('addstudentcontent');
 function loadcontent(content){
     var subcontent=document.getElementsByClassName("subcontent");
